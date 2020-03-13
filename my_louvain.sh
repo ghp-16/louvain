dcd5e381fc2ea9dceab703d02127c5eeb2a071e0
ghp-16 <1397195663@qq.com>
2020-03-13 09:58:17 +0800

    turn to gpu1

--- my_louvain.sh
+++ my_louvain.sh
@@ -1,18 +1,18 @@
 for i in {1..5}
 do
     echo "<====     my_louvain  livejournal     ====>"
-    ./bin/my_louvain --input_dir /data/disk1/clk/livejournal-txt/ --output_dir /home/ghp/GeminiLite/out/
+    ./bin/my_louvain --input_dir /data/disk4/clk/testdata/livejournal-4.85M/txt-no-edge/ --output_dir ~/GeminiLite/output/
+
 done
 
 for i in {1..5}
 do
     echo "<====     my_louvain  facebook     ====>"
-    ./bin/my_louvain --input_dir /data/disk1/clk/facebook-txt/ --output_dir /home/ghp/GeminiLite/out/
+    ./bin/my_louvain --input_dir /data/disk4/clk/testdata/facebook-4039/txt_no_edge/ --output_dir ~/GeminiLite/output/
 done
-./bin/async_louvain --input_dir /home/ghp/twitter/ --output_dir /home/ghp/GeminiLite/out/
 for i in {1..5}
 do
     echo "<====     my_louvain  twitter-half     ====>"
-    ./bin/my_louvain --input_dir /home/ghp/twitter/ --output_dir /home/ghp/GeminiLite/out/
+    ./bin/my_louvain --input_dir /data/disk4/clk/testdata/twitter/twitter-txt/ --output_dir ~/GeminiLite/output/
 done
 
